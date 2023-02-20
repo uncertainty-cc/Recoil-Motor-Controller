ad12b79e1c8a9ee5eedb33729854673684193678
-T.K.- <t_k_233@outlook.com>
2023-02-19 16:52:32 -0800

    ADD: minor change

--- firmware/CAN-USB-Adapter-F446/Core/Src/main.c
+++ firmware/CAN-USB-Adapter-F446/Core/Src/main.c
@@ -283,6 +283,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
